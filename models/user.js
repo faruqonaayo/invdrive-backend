1d1b3d2ab6a428a4fe51c2e04fc008ce77846164
Faruq <onaayofaruq24@gmail.com>
2024-11-09 18:30:58 -0700

    Add user.js file in the models folder

---
+++ models/user.js
@@ -0,0 +1,34 @@
+// importing 3rd party modules
+import mongoose from "mongoose";
+
+// importing custom modules
+
+// creating a schema
+const Schema = mongoose.Schema;
+
+// creating a user schema
+const userSchema = new Schema({
+  firstName: {
+    type: String,
+    required: true,
+  },
+  lastName: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  habitTokens: {
+    type: Number,
+    default: 15000,
+  },
+});
+
+//  exporting the model
+export default mongoose.model("user", userSchema);
